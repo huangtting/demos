345298d48713b022ba7986c797f58439d64d8c5a
huangtingting <huangtt29@mail2.sysu.edu.cn>
2018-12-22 16:30:02 +0800

    添加了eslint和editorconfig的配置

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "root": true,
+    "plugins": [
+      "html"
+    ],
+    "globals": {
+      "window": false,
+      "document": false,
+      "$": false,
+      "XMLHttpRequest": false
+    },
+    "rules": {
+      "no-console": "off",
+      "no-plusplus": "off",
+      'func-names': "off",
+
+    },
+
+};
